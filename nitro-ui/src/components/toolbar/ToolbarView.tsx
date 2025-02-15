7410c1020e26efbf88044185143cbe93222c1494
chrismpettyjohn <chrismpettyjohn@gmail.com>
2025-02-15 15:51:46 -0500

    design: chat bar

--- nitro-ui/src/components/toolbar/ToolbarView.tsx
+++ nitro-ui/src/components/toolbar/ToolbarView.tsx
@@ -78,7 +78,7 @@ export const ToolbarView: FC<{ isInRoom: boolean }> = (props) => {
         <ToolbarMeView useGuideTool={useGuideTool} unseenAchievementCount={getTotalUnseen} setMeExpanded={setMeExpanded} />
       </TransitionAnimation>
       <Flex alignItems="center" justifyContent="between" gap={2} className="nitro-toolbar py-1 px-3">
-        <Flex gap={2} alignItems="center">
+        <Flex gap={2} alignItems="center" style={{ flex: 1 }}>
           <Flex alignItems="center" gap={2}>
             <Flex center pointer className={"navigation-item item-avatar " + (isMeExpanded ? "active " : "")} onClick={(event) => setMeExpanded(!isMeExpanded)}>
               <LayoutAvatarImageView figure={userFigure} direction={2} position="absolute" />
