dee400e9ea28ec7ca9da20c95a06c51db04c1e31
chrismpettyjohn <chrismpettyjohn@gmail.com>
2025-02-17 19:14:50 -0500

    design: crab load

--- nitro-ui/src/components/loading/LoadingView.tsx
+++ nitro-ui/src/components/loading/LoadingView.tsx
@@ -1,35 +1,45 @@
-import { FC } from 'react';
-import { Base, Column, LayoutProgressBar, Text } from '../../common';
+import { FC, useState, useEffect } from "react";
+import { Base, Column, Text } from "../../common";
 
-interface LoadingViewProps
-{
-    isError: boolean;
-    message: string;
-    percent: number;
+interface LoadingViewProps {
+  percent: number;
 }
 
-export const LoadingView: FC<LoadingViewProps> = props =>
-{
-    const { isError = false, message = '', percent = 0 } = props;
-    
-    return (
-        <Column fullHeight position="relative" className="nitro-loading">
-            <Base fullHeight className="container h-100">
-                <Column fullHeight alignItems="center" justifyContent="end">
-                    <Base className="connecting-duck" />
-                    <Column size={ 6 } className="text-center py-4">
-                        { isError && (message && message.length) ?
-                            <Base className="fs-4 text-shadow">{ message }</Base>
-                            :
-                            <>
-                                <Text fontSize={ 4 } variant="white" className="text-shadow">{ percent.toFixed() }%</Text>
-                                <LayoutProgressBar progress={ percent } className="mt-2 large" />
-                            </>
-                        }
-                        
-                    </Column>
-                </Column>
-            </Base>
+const tips = [
+  "Crabs never sleep... neither do Habbo traders.",
+  "If you see a crab, say '🦀' in global chat.",
+  "Fun fact: This crab has more pixels than your furniture collection.",
+  "Tip: The secret to Habbo success? Never trust a free giveaway.",
+  "You are now 80% more crabby. Keep waiting.",
+  "Some say crabs rule the Hotel... Some say it's a conspiracy.",
+];
+
+export const LoadingView: FC<LoadingViewProps> = ({ percent = 0 }) => {
+  const [tipIndex, setTipIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTipIndex((prevIndex) => (prevIndex + 1) % tips.length);
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Column fullHeight position="relative" className="nitro-loading">
+      <Base fullHeight className="container h-100">
+        <Column fullHeight alignItems="center" justifyContent="end">
+          <Base className="connecting-crab" />
+          <Column size={6} className="text-center py-4">
+            <Text fontSize={2} variant="white" className="text-shadow loading-percent">
+              {percent.toFixed()}%
+            </Text>
+            <Text fontSize={2} variant="white" className="text-shadow loading-tip">
+              {tips[tipIndex]}
+            </Text>
+          </Column>
         </Column>
-    );
-}
+      </Base>
+    </Column>
+  );
+};
